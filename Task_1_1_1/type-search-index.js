7c5eb5b7de58babaab70ced4377007834b66e1a1
vaidy31 <vaidy31@users.noreply.github.com>
2025-09-08 02:06:06 +0000

    Deploying to gh-pages from @ vaidy31/OOP@cb6c0db29f7b4e7e4aecf1b33f41104612710c0b 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.mzaugolnikov.Task_1_1_1","l":"Sort"},{"p":"ru.nsu.mzaugolnikov.Task_1_1_1","l":"Task_1_1_1"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.mzaugolnikov.task111","l":"task111"}];updateSearchResults();
