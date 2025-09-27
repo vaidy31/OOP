5f91b44a86456eae9b06fc219d07cc6b093aade8
Maxim Zaugolnikov <m.zaugolnikov@g.nsu.ru>
2025-09-27 19:14:13 +0700

    review changes

--- Task_1_1_2/src/test/java/ru/nsu/mzaugolnikov/task222/BlackjackGameTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/mzaugolnikov/task222/BlackjackGameTest.java
@@ -1,18 +1,14 @@
 package ru.nsu.mzaugolnikov.task222;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
-
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class BlackjackGameTest {
     private ByteArrayOutputStream outputStream;
