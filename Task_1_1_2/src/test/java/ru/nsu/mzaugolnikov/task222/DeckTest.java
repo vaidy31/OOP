f42db1429da863064c0031f9da43a0aee8e0e21f
Maxim Zaugolnikov <m.zaugolnikov@g.nsu.ru>
2025-09-27 16:47:58 +0700

    add tests for DEDCK

---
+++ Task_1_1_2/src/test/java/ru/nsu/mzaugolnikov/task222/DeckTest.java
@@ -0,0 +1,51 @@
+package ru.nsu.mzaugolnikov.task222;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest {
+
+    @Test
+    void testDeckCreation() {
+        Deck deck = new Deck();
+        int count = 0;
+        Cards.Suit[] suits = Cards.Suit.values();
+        Cards.Rank[] ranks = Cards.Rank.values();
+
+        for (Cards.Suit suit : suits) {
+            for (Cards.Rank rank : ranks) {
+                count++;
+            }
+        }
+        assertEquals(count, 52);
+    }
+
+    @Test
+    void testDealCardNotNull() {
+        Deck deck = new Deck();
+        Cards.Card card = deck.dealCard();
+        assertNotNull(card);
+    }
+
+    @Test
+    void testDeckSizeAfter() {
+        Deck deck = new Deck();
+        int initialSize = 52;
+        deck.dealCard();
+        deck.dealCard();
+        deck.dealCard();
+        assertEquals(initialSize - 3, 49);
+    }
+
+    @Test
+    void testDealAll() {
+        Deck deck = new Deck();
+        Cards.Card lastCard = null;
+        for (int i = 0; i < 52; i++) {
+            lastCard = deck.dealCard();
+            assertNotNull(lastCard);
+        }
+        assertNull(deck.dealCard());
+    }
+}
