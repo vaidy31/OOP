9b3b8a4f080e40ac9eaef392406a0b205a306dc1
Maxim Zaugolnikov <m.zaugolnikov@g.nsu.ru>
2025-09-27 17:19:45 +0700

    woof woof @@@@, почему.........javav

--- Task_1_1_2/src/test/java/ru/nsu/mzaugolnikov/task222/HandTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/mzaugolnikov/task222/HandTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 
 class HandTest {
