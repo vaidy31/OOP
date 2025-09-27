e6c9dc1ffe86509acf4b5b1e1febe387e3bf8232
Maxim Zaugolnikov <m.zaugolnikov@g.nsu.ru>
2025-09-27 16:33:30 +0700

    adding some testing tests lol

--- Task_1_1_2/src/main/java/ru/nsu/mzaugolnikov/task222/Hand.java
+++ Task_1_1_2/src/main/java/ru/nsu/mzaugolnikov/task222/Hand.java
@@ -60,7 +60,7 @@ public abstract class Hand {
     }
 
     /**
-     * Блэкджек?
+     * БлэкДжек или нет.
      *
      * @return true/false
      */
--- Task_1_1_2/src/test/java/ru/nsu/mzaugolnikov/task222/BlackjackGameTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/mzaugolnikov/task222/BlackjackGameTest.java
@@ -2,20 +2,54 @@ package ru.nsu.mzaugolnikov.task222;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class BlackjackGameTest {
+
+class BlackjackTest {
 
     @Test
-    public void testCardCreation() {
-        Cards.Card card = new Cards.Card(Cards.Rank.ACE, Cards.Suit.HEARTS);
-        assertNotNull(card);
-        assertEquals(Cards.Rank.ACE, card.getRank());
+    void testCardValue() {
+        Cards.Card card = new Cards.Card(Cards.Rank.KING, Cards.Suit.HEARTS);
+        assertEquals(10, card.getRank().getValue());
+        assertEquals("Король", card.getRank().getName());
     }
 
     @Test
-    public void testDeckInitialization() {
+    void testAceCounting() {
+        Player player = new Player("Игрок");
+        player.addCard(new Cards.Card(Cards.Rank.ACE, Cards.Suit.HEARTS));
+        player.addCard(new Cards.Card(Cards.Rank.NINE, Cards.Suit.CLUBS));
+        assertEquals(20, player.getTotal()); // туз считается как 11
+        player.addCard(new Cards.Card(Cards.Rank.THREE, Cards.Suit.SPADES));
+        assertEquals(13, player.getTotal()); // туз 1
+    }
+
+    @Test
+    void testBlackjack() {
+        Player player = new Player("Игрок");
+        player.addCard(new Cards.Card(Cards.Rank.ACE, Cards.Suit.HEARTS));
+        player.addCard(new Cards.Card(Cards.Rank.KING, Cards.Suit.SPADES));
+        assertTrue(player.isBlackjack());
+    }
+
+    @Test
+    void testBusted() {
+        Player player = new Player("Игрок");
+        player.addCard(new Cards.Card(Cards.Rank.KING, Cards.Suit.HEARTS));
+        player.addCard(new Cards.Card(Cards.Rank.QUEEN, Cards.Suit.CLUBS));
+        player.addCard(new Cards.Card(Cards.Rank.TWO, Cards.Suit.SPADES));
+        assertTrue(player.isBusted());
+    }
+
+    @Test
+    void testDeckDealsCards() {
         Deck deck = new Deck();
-        assertNotNull(deck);
+        int sizeBefore = deck.size();
+        Cards.Card card = deck.dealCard();
+        assertNotNull(card);
+        assertEquals(sizeBefore - 1, deck.size()); // проверка уменьшения колоды
     }
 }
