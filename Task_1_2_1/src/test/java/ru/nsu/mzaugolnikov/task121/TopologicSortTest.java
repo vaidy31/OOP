e2ce0e88bb6e09197fb0acbe6d7c05af32f0e3fb
Maxim Zaugolnikov <m.zaugolnikov@g.nsu.ru>
2025-11-02 01:53:31 +0700

    the end

--- Task_1_2_1/src/test/java/ru/nsu/mzaugolnikov/task121/TopologicSortTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/mzaugolnikov/task121/TopologicSortTest.java
@@ -2,6 +2,9 @@ package ru.nsu.mzaugolnikov.task121;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 
 import java.io.File;
 import java.io.FileWriter;
@@ -65,6 +68,30 @@ public class TopologicSortTest {
         for (Graph graph : graphs) {
             graph.readGraphFromFile(tempFile.getAbsolutePath());
             assertThrows(IllegalArgumentException.class, () -> TopologicSort.sort(graph));
+        }
+        tempFile.delete();
+    }
+
+    @Test
+    void testTopologicSortStrangeAndSpooky() throws IOException {
+        // Создаем временный файл с графом
+        File tempFile = File.createTempFile("graph_goidatest", ".txt");
+        try (FileWriter wr = new FileWriter(tempFile)) {
+            wr.write("1\n"); // количество вершин указываем неверно
+            // для проверки расширяемости объектов хранения данных
+            wr.write("a 2\n");
+            wr.write("1 b\n");
+            wr.write("2 3c\n");
+        }
+        Graph[] graphs = new Graph[]{
+                new AdjacencyListGraph(),
+                new AdjacencyMatrixGraph(),
+                new IncidenceMatrixGraph()
+        };
+
+        for (Graph graph : graphs) {
+            graph.readGraphFromFile(tempFile.getAbsolutePath());
+            assertTrue(graph.vertexSet().isEmpty());
         }
         tempFile.delete();
     }
