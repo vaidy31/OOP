f62bc572b4529da91f62d0a58f346a610266dce5
Maxim Zaugolnikov <m.zaugolnikov@g.nsu.ru>
2025-11-02 01:36:33 +0700

    nobody can help me

--- Task_1_2_1/src/test/java/ru/nsu/mzaugolnikov/task121/AdjacencyListGraphTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/mzaugolnikov/task121/AdjacencyListGraphTest.java
@@ -5,13 +5,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
 /**
  * Тест для класса AdjListGraph.
  */
