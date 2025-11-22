71b1af40f7bee91c3380fa6c24e9245da86af7d9
Maxim Zaugolnikov <m.zaugolnikov@g.nsu.ru>
2025-11-22 20:37:09 +0700

    init x4

--- Task_1_2_2/src/test/java/ru/nsu/mzaugolnikov/task122/HashTableTest.java
+++ Task_1_2_2/src/test/java/ru/nsu/mzaugolnikov/task122/HashTableTest.java
@@ -1,17 +1,17 @@
 package ru.nsu.mzaugolnikov.task122;
 
+import java.util.ConcurrentModificationException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.ConcurrentModificationException;
-
 /**
  * Тесты для того чтобы все было надежно.
  */
