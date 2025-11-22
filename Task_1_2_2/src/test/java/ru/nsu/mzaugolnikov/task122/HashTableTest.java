7c24654620c674ce63630326c331dafe8f2927f0
Maxim Zaugolnikov <m.zaugolnikov@g.nsu.ru>
2025-11-22 20:59:07 +0700

    end version

--- Task_1_2_2/src/test/java/ru/nsu/mzaugolnikov/task122/HashTableTest.java
+++ Task_1_2_2/src/test/java/ru/nsu/mzaugolnikov/task122/HashTableTest.java
@@ -2,8 +2,8 @@ package ru.nsu.mzaugolnikov.task122;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
